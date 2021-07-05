f7c433356a26aeda8a2d7aff63f37be637914a94
liuboringbook <1287128847@qq.com>
2021-07-05 20:37:47 +0800

    index

---
+++ components/public/header/topbar.vue
@@ -0,0 +1,31 @@
+<template>
+  <el-row class="m-header" :gutter="0">
+       <el-col :span="5">
+         <geo />
+       </el-col>
+       <el-col :span="9">
+         <user/>
+       </el-col>
+       <el-col :span="10">
+         <nav-bar></nav-bar>
+       </el-col>
+  </el-row>
+</template>
+
+<script>
+import Geo from './geo'
+import User from './user'
+import NavBar from './nav'
+  export default {
+    components: {
+      Geo,
+      User,
+      NavBar
+    }
+  }
+</script>
+
+<style lang="scss">
+  @import "@/assets/css/public/header/index.scss";
+  @import "@/assets/css/public/layout.scss";
+</style>
